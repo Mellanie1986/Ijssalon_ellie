929d2da80a61abb82e4b9d121a6fe233b125ef4e
Mellanie van Est <mellanievanest01@gmail.com>
2025-03-10 15:11:12 +0100

    start tijdelijk.py

---
+++ tijdelijk.py
@@ -0,0 +1,6 @@
+prijzen = {"aarbeien":"3",
+           "vanille":"4",
+           "chocolade":"5" }
+aanbieding = (0,8 * 3)
+reclame_tekst = "Vandaag in de aanbieding :Vanille_ijs, 1 Liter - slechts €<aanbieding> "
+print("reclame_tekst")
